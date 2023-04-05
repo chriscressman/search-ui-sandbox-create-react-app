4f34971fd0e0057d1c86f0da5409a5ca00f906b6
Chris Cressman <chris@chriscressman.com>
2023-04-05 15:51:02 -0400

    Implement minimal Search UI app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import AppSearchAPIConnector from '@elastic/search-ui-app-search-connector';
+import { SearchProvider, WithSearch } from '@elastic/react-search-ui';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const endpointBase = 'https://enterprise-search-8-6-1-4gb-ram.ent.us-central1.gcp.cloud.es.io';
+const engineName = 'content-site';
+const searchKey = 'search-se4wspv888i3itjnqghpm954';
+const apiConnectorConfig = { endpointBase, engineName, searchKey };
+const apiConnector = new AppSearchAPIConnector(apiConnectorConfig);
+const searchProviderConfig = { apiConnector };
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);
+
+function App () {
+  console.log('Inside <App>');
+  return (
+    <SearchProvider config={searchProviderConfig}>
+      <WithSearch mapContextToProps={mapContextToProps}>{children}</WithSearch>
+    </SearchProvider>
+  );
+}
+
+function mapContextToProps (context) {
+  const logMessage =
+    `Inside mapContextToProps() for <WithSearch>', typeof context.trackClickThrough is: ${typeof context.trackClickThrough}`
+  console.log(logMessage);
+  return {};
+}
+
+function children (props) {
+  const logMessage = 'Inside children';
+  console.log(logMessage);
+  return null;
+}
